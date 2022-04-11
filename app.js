0d485cbb12709076245f31594107dbac9b911b7b
Janae Edwards <edwards.janae@gmail.com>
2022-04-11 12:08:09 -0400

    notes on authentication steps, before JWT

--- app.js
+++ app.js
@@ -6,15 +6,31 @@ const path = require('path');
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
+/*
+2.
+This POST request is sent after the initial sign in. the log in credentials that the user inputs are compiled as a req.body.
+Once the server has this req.body, it then calls this "authenticate" function (in db.js), with that req.body/credentials as the input.
+*/
 app.post('/api/auth', async(req, res, next)=> {
+  // req.body expected to be {username: "moe", password: "moe_pw"}
   try {
-    res.send({ token: await User.authenticate(req.body)});
+    res.send({ token: await User.authenticate(req.body)}); 
+    /* 
+    what is returned from the authenticate function in db.js, will look like "{token: 1}" for example. 
+    in the network tab, you will find this in "Preview" tab. 
+    */
   }
   catch(ex){
     next(ex);
   }
 });
-
+/*
+6.  
+this GET request is initiated by the attemptTokenLogin function.
+we send the GET request with the token in order to get additional information about the user.
+this GET request takes in the req.headers.authorization, which in the network tab under Request Headers, is the user.id (will be a JWT token later).
+this route calls ANOTHER class method, called byToken (in db.js), with this token (which is returned as headers.authorization).
+*/
 app.get('/api/auth', async(req, res, next)=> {
   try {
     res.send(await User.byToken(req.headers.authorization));
--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const Sequelize = require('sequelize');
 const { STRING } = Sequelize;
 const config = {
@@ -14,9 +15,25 @@ const User = conn.define('user', {
   password: STRING
 });
 
+/*
+7. 
+this byToken class method (function) is called by the api GET request. 
+this function takes in the token as its argument, and its purpose is the try to find the user via this token.
+we use User.findByPk(token) to locate this user.
+if there is a user, we return this user.
+if not, we throw the 401 error. 
+the user data that is returned is then sent back to attempttokenLogin (which made the initial GET request in the first place),
+and calls setState with this response (user) data. in the "auth" object.
+this.state = {
+  auth: {
+    username: username, 
+    password: password
+  }
+}
+*/
 User.byToken = async(token)=> {
   try {
-    const user = await User.findByPk(token);
+    const user = await User.findByPk(token); // since the token is currently the user.id, we can use findByPk. 
     if(user){
       return user;
     }
@@ -31,6 +48,15 @@ User.byToken = async(token)=> {
   }
 };
 
+/*
+3.
+the POST request calls this function with the user's credentials that it receives during the signIn step.
+This authenticate function takes in the username and password as arguments. we need both bc we need to authenticate BOTH the username AND the password.
+We use this data to findOne in the User model (below). 
+IF there user exists already, i.e. if User.findOne({}) returns a user from our database, we return the user.id (for now, but this will change later w/ JWT)
+If the log in credentials are inaccurate, OR the user cannot be found, this function will throw a 401 error.
+The user.id that is returned (if successful), will then be sent BACK from the server (back in the POST request) as the token (for now).
+*/
 User.authenticate = async({ username, password })=> {
   const user = await User.findOne({
     where: {
